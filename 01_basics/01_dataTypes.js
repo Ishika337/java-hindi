f9d550eee3c0a852e1166036c76472884a93b2cd
Ishika337 <missiitianiitjammu26@gmail.com>
2025-03-11 20:39:09 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" ; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3
+    +3)  // code readibility should be high
+
+
+let name = "hitesh"
+let age = 18 
+let loggedIn = false 
+let state ;
+
+// number => 2to power 53 (range)
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined  example : let state ;
+// symbol => unique value 
+
+// objects 
+
+console.log(typeof "ishika")
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "ishika@gmail.com" 
+// var accountPassword = "12345"  using var is not a good practice insted use let or because of issue of block scope and functional scope
+accountCity = "Jaipur"
+//  let accountState ;  it will show undefined when we doesnot give them any value
+
+// accountId = 2  not change as weassign it as const varable 
+
+//  we can use let aur var for creating varibles delcaration
+
+// console.log(accountId)
+
+accountEmail = "ishi@gmail.com"
+accountPassword = "22334"
+accountCity = "hisar"
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
+
