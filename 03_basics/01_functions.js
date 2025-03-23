6f7c927352709aa8d22e943852dd6437b20c19a3
Ishika337 <missiitianiitjammu26@gmail.com>
2025-03-23 19:01:02 +0000

    scope1 done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,12 +1,12 @@
 
 function sayMyName(){
 
-console.log("i");
-console.log("s");
-console.log("h");
-console.log("i");
-console.log("k");
-console.log("a");
+// console.log("i");
+// console.log("s");
+// console.log("h");
+// console.log("i");
+// console.log("k");
+// console.log("a");
 
 }
 sayMyName() // saymyname is reference here and () it is exceution
@@ -34,11 +34,66 @@ console.log("result :" , result) ; // isse value print hoti h na ki return
 function loginUserMessage(username) {
   if(username === undefined)
   {
-    console.log("please enter username")
+   // console.log("please enter username")
   }
 
     return '${username} just logged in' 
 }
 
-console.log(loginUserMessage("ishika")) ;
-console.log(loginUserMessage()) ;
+// console.log(loginUserMessage("ishika")) ;
+// console.log(loginUserMessage()) ;
+
+
+// function calculateCartPrice(num1) {
+
+//   return num1
+// }
+
+// console.log(calculateCartPrice(200 , 400)) ; // if we want toaceess all the items written inside then we have to use spread/rest(...) operator
+
+
+
+// function calculateCartPrice(...num1) {
+
+//   return num1
+// }
+
+// console.log(calculateCartPrice(200 , 400)) ;
+
+
+function calculateCartPrice( val1 , val2 ,...num1) {
+
+  return num1
+}
+
+ //console.log(calculateCartPrice(200 , 400 , 2000 , 500)) ;
+
+
+const user  = {
+
+  username : "hitesh" ,
+  price : 199 
+}
+
+function handleObject(anyobject){
+
+  console.log('Username is ${anyobject.username} and price is ${anyobject.price} ') ;
+}
+
+ //handleObject(user) 
+
+ handleObject ({
+
+  username : "sam" ,
+  price : 399
+ })
+
+ const myNewArray = [200 , 400 , 100 , 600]
+
+ function returnSecondValue(getArray) {
+
+  return getArray[1]
+ }
+
+ //console.log(returnSecondValue(myNewArray)) ;
+ console.log(returnSecondValue([200 ,400 , 500])) ;
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+
+// let a = 10 
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+let a = 300
+if(true) {
+
+let a = 10 
+const b = 20
+var c = 30
+ console.log("inside :" , a) ;
+}
+
+// console.log(a); // a will not print as it scope is only inside if
+// console.log(b); // similarly for b
+console.log(c); // var cant be used as we scope it into if but it is running outside also
+
+
+
